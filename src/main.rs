a26af86fea142a634ef1d64b9377d74401aa0cb9
oliver <oliver.pecek@gmail.com>
2024-05-06 10:56:06 +0200

    fmt infos refactored

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod ascii;
 
 fn main() {
     let logo = ascii::get_logo(info::get_os_type());
-    println!("{}", info::fmt_info());
     println!("{}", logo);
+    println!("{}", info::fmt_info_header());
+    println!("{}", info::fmt_info());
 }
