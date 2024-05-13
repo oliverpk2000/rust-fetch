9ee58c822508725efc9edc2f4f4306ea15d3fd60
oliver <oliver.pecek@gmail.com>
2024-05-13 10:12:15 +0200

    clippy compliant

--- src/ascii.rs
+++ src/ascii.rs
@@ -2,66 +2,66 @@ use colored::{ColoredString, Colorize};
 use os_info::Type;
 
 pub fn get_logo(os_type: Type) -> ColoredString {
-    match os_type {
-        Type::AIX => { return "TODO".to_string().red(); }
-        Type::AlmaLinux => { return "TODO".to_string().red(); }
-        Type::Alpaquita => { return "TODO".to_string().red(); }
-        Type::Alpine => { return "TODO".to_string().red(); }
-        Type::Amazon => { return "TODO".to_string().red(); }
-        Type::Android => { return "TODO".to_string().red(); }
-        Type::Arch => { return "TODO".to_string().red(); }
-        Type::Artix => { return "TODO".to_string().red(); }
-        Type::CentOS => { return "TODO".to_string().red(); }
+    return match os_type {
+        Type::AIX => { "TODO".to_string().red() }
+        Type::AlmaLinux => { "TODO".to_string().red() }
+        Type::Alpaquita => { "TODO".to_string().red() }
+        Type::Alpine => { "TODO".to_string().red() }
+        Type::Amazon => { "TODO".to_string().red() }
+        Type::Android => { "TODO".to_string().red() }
+        Type::Arch => { "TODO".to_string().red() }
+        Type::Artix => { "TODO".to_string().red() }
+        Type::CentOS => { "TODO".to_string().red() }
         Type::Debian => {
             let debian_logo = include_str!("logos/debian.ascii");
-            return debian_logo.to_string().red();
+            debian_logo.to_string().red()
         }
-        Type::DragonFly => { return "TODO".to_string().red(); }
-        Type::Emscripten => { return "TODO".to_string().red(); }
-        Type::EndeavourOS => { return "TODO".to_string().red(); }
+        Type::DragonFly => { "TODO".to_string().red() }
+        Type::Emscripten => { "TODO".to_string().red() }
+        Type::EndeavourOS => { "TODO".to_string().red() }
         Type::Fedora => {
             let fedora_logo = include_str!("logos/fedora.ascii");
-            return fedora_logo.to_string().blue();
+            fedora_logo.to_string().blue()
         }
-        Type::FreeBSD => { return "TODO".to_string().red(); }
-        Type::Garuda => { return "TODO".to_string().red(); }
-        Type::Gentoo => { return "TODO".to_string().red(); }
-        Type::HardenedBSD => { return "TODO".to_string().red(); }
-        Type::Illumos => { return "TODO".to_string().red(); }
-        Type::Kali => { return "TODO".to_string().red(); }
-        Type::Linux => { return "TODO".to_string().red(); }
-        Type::Mabox => { return "TODO".to_string().red(); }
-        Type::Macos => { return "TODO".to_string().red(); }
-        Type::Manjaro => { return "TODO".to_string().red(); }
-        Type::Mariner => { return "TODO".to_string().red(); }
-        Type::MidnightBSD => { return "TODO".to_string().red(); }
-        Type::Mint => { return "TODO".to_string().red(); }
-        Type::NetBSD => { return "TODO".to_string().red(); }
-        Type::NixOS => { return "TODO".to_string().red(); }
-        Type::OpenBSD => { return "TODO".to_string().red(); }
-        Type::OpenCloudOS => { return "TODO".to_string().red(); }
-        Type::openEuler => { return "TODO".to_string().red(); }
-        Type::openSUSE => { return "TODO".to_string().red(); }
-        Type::OracleLinux => { return "TODO".to_string().red(); }
-        Type::Pop => { return "TODO".to_string().red(); }
-        Type::Raspbian => { return "TODO".to_string().red(); }
-        Type::Redhat => { return "TODO".to_string().red(); }
-        Type::RedHatEnterprise => { return "TODO".to_string().red(); }
-        Type::Redox => { return "TODO".to_string().red(); }
-        Type::RockyLinux => { return "TODO".to_string().red(); }
-        Type::Solus => { return "TODO".to_string().red(); }
-        Type::SUSE => { return "TODO".to_string().red(); }
+        Type::FreeBSD => { "TODO".to_string().red() }
+        Type::Garuda => { "TODO".to_string().red() }
+        Type::Gentoo => { "TODO".to_string().red() }
+        Type::HardenedBSD => { "TODO".to_string().red() }
+        Type::Illumos => { "TODO".to_string().red() }
+        Type::Kali => { "TODO".to_string().red() }
+        Type::Linux => { "TODO".to_string().red() }
+        Type::Mabox => { "TODO".to_string().red() }
+        Type::Macos => { "TODO".to_string().red() }
+        Type::Manjaro => { "TODO".to_string().red() }
+        Type::Mariner => { "TODO".to_string().red() }
+        Type::MidnightBSD => { "TODO".to_string().red() }
+        Type::Mint => { "TODO".to_string().red() }
+        Type::NetBSD => { "TODO".to_string().red() }
+        Type::NixOS => { "TODO".to_string().red() }
+        Type::OpenBSD => { "TODO".to_string().red() }
+        Type::OpenCloudOS => { "TODO".to_string().red() }
+        Type::openEuler => { "TODO".to_string().red() }
+        Type::openSUSE => { "TODO".to_string().red() }
+        Type::OracleLinux => { "TODO".to_string().red() }
+        Type::Pop => { "TODO".to_string().red() }
+        Type::Raspbian => { "TODO".to_string().red() }
+        Type::Redhat => { "TODO".to_string().red() }
+        Type::RedHatEnterprise => { "TODO".to_string().red() }
+        Type::Redox => { "TODO".to_string().red() }
+        Type::RockyLinux => { "TODO".to_string().red() }
+        Type::Solus => { "TODO".to_string().red() }
+        Type::SUSE => { "TODO".to_string().red() }
         Type::Ubuntu => {
             let ubuntu_logo = include_str!("logos/ubuntu.ascii");
-            return ubuntu_logo.to_string().truecolor(255, 150, 0);
+            ubuntu_logo.to_string().truecolor(255, 150, 0)
         }
-        Type::Ultramarine => { return "TODO".to_string().red(); }
-        Type::Void => { return "TODO".to_string().red(); }
-        Type::Unknown => { return "TODO".to_string().red(); }
+        Type::Ultramarine => { "TODO".to_string().red() }
+        Type::Void => { "TODO".to_string().red() }
+        Type::Unknown => { "TODO".to_string().red() }
         Type::Windows => {
             let windows_logo = include_str!("logos/windows.ascii");
-            return windows_logo.to_string().blue();
+            windows_logo.to_string().blue()
         }
-        _ => { return "TODO: error logo".to_string().red(); }
+        _ => { "TODO: error logo".to_string().red() }
     }
 }
